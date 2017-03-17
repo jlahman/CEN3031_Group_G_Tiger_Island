eb2f577010d526429f3626bd46a5f9cb65dc8acf
o9dangson <o9dangson@gmail.com>
2017-03-16 23:30:32 -0400

    Update Clock.java

--- IdeaProjects/CEN3031/src/Clock.java
+++ IdeaProjects/CEN3031/src/Clock.java
@@ -1,14 +1,14 @@
 public class Clock{
-	private static boolean turnCounter= true;
+    private static boolean turnCounter= true;
 
-	public static void increment(){
-		this.turnCounter=!(this.turnCounter);
-	}
-	
-	public static void displayCounter(){
-		if(this.turnCounter)
-			System.out.println("White");
-		else if(!(this.turnCounter))
-			System.out.println("Black");
-	}
+    public void increment(){
+        this.turnCounter=!(this.turnCounter);
+    }
+
+    public void displayCounter(){
+        if(this.turnCounter)
+            System.out.println("White");
+        else if(!(this.turnCounter))
+            System.out.println("Black");
+    }
 }
