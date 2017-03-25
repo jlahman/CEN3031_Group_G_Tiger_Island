30daa8a201ffba0bce8666048bea90e6c0f6123e
Harrison Magoutas <hmagoutas@ufl.edu>
2017-03-25 14:06:17 -0400

    Added tests
    Added tests for setter methods.

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,11 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by madashi on 3/24/17.
- */
 public class PlayerTest {
+    Player p;
 
+    @Before
+    public void setUp() throws Exception {
+        p = new Player();
+    }
 
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void setMeepleCount() throws Exception {
+        p = new Player();
+        p.setMeepleCount(19);
+        assertEquals(p.getMeepleCount(),19);
+    }
+
+    @Test
+    public void setTotoroCount() throws Exception {
+        p = new Player();
+        p.setTotoroCount(19);
+        assertEquals(p.getTotoroCount(),19);
+    }
+
+    @Test
+    public void setTigerCount() throws Exception {
+        p = new Player();
+        p.setTigerCount(19);
+        assertEquals(p.getTigerCount(),19);
+    }
 }
