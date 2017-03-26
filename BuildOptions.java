33abd7d0c53f23b63f7eb4c8f7b9005717c098af
hmagoutas <harrison.magoutas@gmail.com>
2017-03-25 20:41:47 -0400

    Added skeleton for buildSettlement function

--- BuildOptions.java
+++ BuildOptions.java
@@ -2,5 +2,18 @@
  * Created by Harrison on 3/25/2017.
  */
 public class BuildOptions {
-    public void
+
+    // Build settlement
+    public void buildSettlement (Hex h) {
+        // code
+    }
+
+    // Expand settlement
+        // code
+
+    // Place Totoro
+        // code
+
+    // Build tiger sanctuary
+        // code
 }
