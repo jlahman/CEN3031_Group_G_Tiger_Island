a40bd2f88770a79277b00363d87ed3f6ab7ef92b
Harrison Magoutas <hmagoutas@ufl.edu>
2017-03-25 20:44:54 -0400

    Added empty skeleton for buildSettlement
    Added empty skeleton for buildSettlement with comments for other build options

---
+++ BuildOptions.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Harrison on 3/25/2017.
+ */
+public class BuildOptions {
+
+    // Build settlement
+    public void buildSettlement (Hex h) {
+        // code
+    }
+
+    // Expand settlement
+        // code
+
+    // Place Totoro
+        // code
+
+    // Build tiger sanctuary
+        // code
+}
