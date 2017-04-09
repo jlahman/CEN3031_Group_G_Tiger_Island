4b1afbbc69db60faefee39228da288826f529294
mquach95 <mquach95@ufl.edu>
2017-04-09 18:46:46 -0400

    Add files via upload

---
+++ IdeaProjects/CEN3031/Test/AITest.java
@@ -0,0 +1,35 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mquac on 4/9/2017.
+ */
+public class AITest {
+
+    AI a;
+    @Before
+    public void setUp() throws Exception {
+        a = new AI();
+    }
+
+/*    @After
+    public void tearDown() throws Exception {
+
+    }*/
+
+    @Test
+    public void setGameID() throws Exception {
+        a = new AI();
+        a.setGameID("gameName");
+        assertEquals(a.getGameID(),"gameName");
+    }
+
+    @Test
+    public void playMove() throws Exception {
+        
+    }
+
+}
