099198b844babb775fa65baa3b8af6b0f5f8879b
mquach95 <mquach95@ufl.edu>
2017-04-10 11:17:02 -0400

    parameter error in buildTigerSanctuaryNotOnLevel3Test and buildTigerSanctuaryOnLevel3Test

--- IdeaProjects/CEN3031/Test/PlayerTest.java
+++ IdeaProjects/CEN3031/Test/PlayerTest.java
@@ -11,55 +11,50 @@ import static org.junit.Assert.*;
 
 public class PlayerTest {
     Player p;
+    String name;
 
     @Before
     public void setUp() throws Exception {
-        p = new Player();
+        p = new Player(name);
     }
 
-    @After
+/*    @After
     public void tearDown() throws Exception {
 
-    }
+    }*/
 
     @Test
     public void setSetMeepleCountTest() throws Exception {
-        p = new Player();
         p.setMeepleCount(19);
         assertEquals(p.getMeepleCount(),19);
     }
 
     @Test
     public void setTotoroCountTest() throws Exception {
-        p = new Player();
         p.setTotoroCount(19);
         assertEquals(p.getTotoroCount(),19);
     }
 
     @Test
     public void setTigerCountTest() throws Exception {
-        p = new Player();
         p.setTigerCount(19);
         assertEquals(p.getTigerCount(),19);
     }
 
     @Test
     public void setWrongMeepleCountTest() throws Exception {
-        p = new Player();
         p.setMeepleCount(-1);
         assertFalse(p.getMeepleCount()==-1);
     }
 
     @Test
     public void setWrongTotoroCountTest() throws Exception {
-        p = new Player();
         p.setTotoroCount(-1);
         assertFalse(p.getTotoroCount()==-1);
     }
 
     @Test
     public void setWrongTigerCountTest() throws Exception {
-        p = new Player();
         p.setTigerCount(-1);
         assertFalse(p.getTigerCount()==-1);
     }
