1b91b649c128b9e27300aef35bf87c70c618dbdc
Justin Lahman <jlahman@users.noreply.github.com>
2017-04-14 16:29:10 -0400

    Add files via upload

---
+++ IdeaProjects/CEN3031/Test/Hex.java
@@ -0,0 +1,157 @@
+/**
+ * Created by Justin Lahman on 3/23/17.
+ */
+public class Hex {
+    //Hexes are made up of a terrain and level. Terrain is unchangeable.
+    private final Terrain terrain;
+    private final int N = 6;
+    private int level;
+    //public int getAdjHex[];
+    public int indexX, indexY;
+    //private boolean isEmpty;
+    private Tile tile;
+    private int meeple;
+    private int totoro;
+    private int tiger;
+    private int settlementID;
+
+    private Player owner;
+
+    //The constructor for the hex class
+    public Hex() {
+        level = 0;
+        terrain = null;
+        tile = null;
+        //adjHex = new Hex[N];
+        meeple = 0;
+        totoro = 0;
+        tiger = 0;
+        settlementID = -1;
+        owner = null;
+        indexX = -1;
+        indexY = -1;
+    }
+
+    public Hex(Terrain type) {
+
+        terrain = type;
+        level = 0;
+        tile = null;
+        indexX = -1;
+        indexY = -1;
+        meeple = 0;
+        totoro = 0;
+        tiger = 0;
+        settlementID = -1;
+        owner = null;
+    }
+
+    //Getter for Hex terrain
+    public Terrain getTerrain() {
+        return terrain;
+    }
+
+    public boolean isEmpty() {
+        if(owner == null && meeple == 0 && tiger == 0 && totoro == 0 && settlementID == -1)
+            return true;
+        return false;
+    }
+
+    //Getter for Hex terrain
+    public String getTerrainAsString() {
+        return terrain.getTerrainText();
+    }
+
+    //Getter for level
+    public int getLevel() {
+        return level;
+    }
+
+    //Setter for level
+    public void setLevel(int level) {
+        if(level>0)
+            this.level = level;
+    }
+
+    public Tile getTile() {
+        return tile;
+    }
+
+    public void setTile(Tile parentTile) {
+        this.tile = parentTile;
+    }
+
+    public boolean hasTotoro(){
+      return (totoro == 1);
+    }
+
+    public int getMeeple() {
+        return meeple;
+    }
+
+    public void setMeeple(int meeple) {
+        if(meeple >= 0)
+            this.meeple = meeple;
+    }
+
+    public void addMeeple(Player p){
+        this.meeple = this.level;
+        p.decreaseMeepleCount(this.level);
+    }
+
+    public void addTotoro(Player p){
+        this.totoro = 1;
+        p.decreaseTotoroCount(1);
+    }
+
+    public void addTiger(Player p){
+        this.tiger = 1;
+        p.decreaseTigerCount(1);
+    }
+
+    public int getTotoro() {
+        return totoro;
+    }
+
+    public void setTotoro(int totoro) {
+        if (totoro == 0 || totoro == 1)
+            this.totoro = totoro;
+    }
+
+    public int getTiger() {
+        return tiger;
+    }
+
+    public void setTiger(int tiger) {
+        if(tiger == 0 || tiger == 1)
+            this.tiger = tiger;
+    }
+
+    public boolean hasTiger(){
+       return (tiger == 1);
+    }
+
+    public int getSettlementID() {
+        return settlementID;
+    }
+
+    public void setSettlementID(int settlementID) {
+        this.settlementID = settlementID;
+    }
+
+    public Player getOwner(){
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+}
+
+
+/*
+file new maven create from archt, quckstart
+whateer gid
+perferences
+*/
---
+++ IdeaProjects/CEN3031/Test/Settlement.java
@@ -0,0 +1,76 @@
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * Created by Justin Lahman on 3/23/17.
+ */
+public class Settlement {
+
+    private int settlementID;
+    public List<Hex> hexesInSettlement = new Vector<Hex>();
+
+    public Settlement(Hex hex, int ID){
+        hexesInSettlement.add(hex);
+        settlementID = ID;
+        hex.setSettlementID(settlementID);
+    }
+
+    public Settlement(List<Hex> hexList, int ID, Player P){
+        hexesInSettlement = hexList;
+        settlementID = ID;
+        for (Hex hex : hexesInSettlement){
+            hex.setSettlementID(settlementID);
+            hex.setOwner(P);
+        }
+    }
+
+    public void setSettlementID(int sID){
+        settlementID = sID;
+        for (Hex hex : hexesInSettlement){
+            hex.setSettlementID(settlementID);
+        }
+    }
+
+    public int getSettlementID() {
+        return settlementID;
+    }
+
+    public int settlementSize(){
+        return hexesInSettlement.size();
+    }
+
+    public void addHex(Hex hex){
+        hexesInSettlement.add(hex);
+        hex.setSettlementID(settlementID);
+        hex.setOwner(getOwner());
+    }
+
+    public void removeHex(Hex hex){
+        hexesInSettlement.remove(hex);
+        hex.setSettlementID(-1);
+        hex.setOwner(null);
+    }
+
+    public Player getOwner(){
+        if(hexesInSettlement.size() == 0)
+            return null;
+        return hexesInSettlement.get(0).getOwner();
+    }
+
+    public boolean hasTotoro(){
+        for(Hex h: hexesInSettlement){
+            if(h.hasTotoro())
+                return true;
+        }
+        return false;
+    }
+
+    public boolean hasTiger(){
+        for(Hex h: hexesInSettlement){
+            if(h.hasTiger())
+                return true;
+        }
+        return false;
+    }
+}
